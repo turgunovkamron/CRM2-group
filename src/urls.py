81a14e639c9d5c8ad39b7b78993b4f500404a348
Johadev05 <132278335+Johadev05@users.noreply.github.com>
2023-06-05 19:37:57 +0500

    Update urls.py

--- src/urls.py
+++ src/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("director.urls"))
+    path("api/", include("director.urls"))
 ]
