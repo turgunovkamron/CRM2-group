cb2b86ac4ef95b4589d3af992f8d3ed0476bd99c
Johadev05 <132278335+Johadev05@users.noreply.github.com>
2023-06-05 19:18:34 +0500

    AUTH_USER_MODEL

--- src/settings.py
+++ src/settings.py
@@ -31,14 +31,17 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'director'
+    'director',
+    "rest_framework.authtoken",
 ]
+
+AUTH_USER_MODEL = 'director.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -135,3 +138,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
