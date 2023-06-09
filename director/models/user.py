0e365e91d97d1d698b6419f3e8e9388adbc80da7
kamron <124760661+turgunovkamron@users.noreply.github.com>
2023-06-09 19:11:37 +0500

    new

--- director/models/user.py
+++ director/models/user.py
@@ -2,38 +2,9 @@ from django.contrib.auth.base_user import AbstractBaseUser
 from django.db import models
 
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, UserManager
-
-
-class User(AbstractBaseUser, PermissionsMixin):
-    name = models.CharField(max_length=124, null=True, blank=True)
-    last_name = models.CharField(max_length=128, null=True, blank=True)
-    phone = models.CharField(max_length=131, null=True, blank=True)
-    password = models.CharField(max_length=1029)
-    email = models.EmailField(unique=True)
-    join_date = models.DateField(auto_now_add=True)
-    type = models.SmallIntegerField(default=0)
-
-    is_superuser = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
-
-    def format(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "email": self.email,
-            "join_date": self.join_date,
-            "phone": self.phone,
-            "type": self.type,
-            "last_name": self.last_name
-        }
-
+from django.db import models
 
 class CustomUserManager(UserManager):
-
     def create_user(self, email, password=None, is_staff=False, is_active=True, is_superuser=False,
                     **extra_fields):
         user = self.model(email=email,
@@ -54,15 +25,31 @@ class CustomUserManager(UserManager):
                                 is_active=True,
                                 **extra_fields)
 
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=124, null=True, blank=True)
+    last_name = models.CharField(max_length=128, null=True, blank=True)
+    phone = models.CharField(max_length=131, null=True, blank=True)
+    password = models.CharField(max_length=1029)
+    email = models.EmailField(unique=True)
+    join_date = models.DateField(auto_now_add=True)
+    type = models.SmallIntegerField(default=0)
+
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    objects = CustomUserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     def format(self):
         return {
             "id": self.id,
-            "phone": self.phone,
-            "email": self.email,
             "name": self.name,
-            "username":self.username,
-            "is_staff": self.is_staff,
-            "is_superuser": self.is_superuser,
-            "is_active": self.is_active,
-
+            "email": self.email,
+            "join_date": self.join_date,
+            "phone": self.phone,
+            "type": self.type,
+            "last_name": self.last_name
         }
