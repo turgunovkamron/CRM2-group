9a1a39e721ac24c0fdf8d0159d7a2209fa790d68
kamron <124760661+turgunovkamron@users.noreply.github.com>
2023-06-12 19:46:39 +0500

    a

--- base/send_email.py
+++ base/send_email.py
@@ -1,10 +1,11 @@
+from django.conf import settings
 from django.core.mail import send_mail
 
 
 def send_email(OTP, email):
-    subject = 'Hello'
+    title = 'Hello'
     message = f'One Time Password {OTP}'
-    from_email = 'djangoproject32@gmail.com'
+    from_email = settings.EMAIL_HOST_USER
     recipient_list = [f'{email}']
 
-    send_mail(subject, message, from_email, recipient_list)
+    send_mail(title, message, from_email, recipient_list)
--- src/settings.py
+++ src/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = 'django-insecure-+$v+9)4+1=@ypkphh--s%lst@5a2nirlcj88a88z4!)%=6x@+%
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -75,7 +73,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'src.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
@@ -97,7 +94,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -116,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -128,7 +123,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
@@ -138,7 +132,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
