e04b51d7edf570ee74eb41ef538274740b042bcc
Johadev05 <132278335+Johadev05@users.noreply.github.com>
2023-06-21 22:36:19 +0500

    Update urls.py

--- director/urls.py
+++ director/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 from .views import Main
-from director.methods.auth import user_actions
 urlpatterns = [
     path("main/", Main.as_view()),
-    path("user/actions/", user_actions.as_view()),
 ]
